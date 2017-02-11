4f46197efcefd6b0afb8812608382a60c4b970f0
ivelten <ismaelcarlosvelten@gmail.com>
2017-02-11 10:44:41 -0200

    Modularizing Console.

---
+++ SerebiiParser.Core/AbilityParser.fs
@@ -0,0 +1,16 @@
+﻿namespace SerebiiParser.Core
+
+open FSharp.Data
+open System
+open System.Globalization
+
+module AbilityParser =
+    let baseUrl = "http://serebii.net/abilitydex/"
+    let results = HtmlDocument.Load(baseUrl)
+
+    let abilityCombos =
+        results.Descendants("select")
+            |> Seq.filter (fun s ->
+                s.TryGetAttribute("name")
+                    |> Option.map (fun n -> n.Value())
+                        = Some "SelectURL")
